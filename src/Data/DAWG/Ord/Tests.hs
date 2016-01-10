20e26db86df60731c6f8f50e73c7056281bea820
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2016-01-10 14:56:42 +0100

    Add more tests

--- src/Data/DAWG/Ord/Tests.hs
+++ src/Data/DAWG/Ord/Tests.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+-- {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE RecordWildCards #-}
 
 
 module Data.DAWG.Ord.Tests where
@@ -12,6 +12,35 @@ import           Test.HUnit (Assertion, (@?=))
 import           Test.Tasty.HUnit (testCase)
 
 import qualified Data.DAWG.Ord as D
+
+
+---------------------------------------------------------------------
+-- All Tests
+---------------------------------------------------------------------
+
+
+-- | All tests of the corresponding module.
+tests :: TestTree
+tests = testGroup "Data.DAWG.Ord"
+    [ testCase "fullLang" $ fullLang [0..2::Int] 5
+    , testCase "assocsFromListID" $ assocsFromListID dataSet1
+    , testCase "dataSet1DAWGSize" dataSet1DAWGSize ]
+
+
+---------------------------------------------------------------------
+-- Assertions / Properties
+---------------------------------------------------------------------
+
+
+-- | Verify that `assocs . fromList` is an identity (up to the order
+-- of the elements in the input list).
+assocsFromListID
+    :: (Show a, Ord a, Show b, Ord b)
+    => [([a], b)]
+    -> Assertion
+assocsFromListID xs
+    =   S.fromList (D.assocs (D.fromList xs))
+    @?= S.fromList xs
 
 
 -- | Verify the numbers of states and transitions for a language S^n
@@ -30,7 +59,26 @@ fullLang xList n = do
                 , x <- S.toList xSet]
 
 
--- | All tests of the corresponding module.
-tests :: TestTree
-tests = testGroup "Data.DAWG.Ord"
-    [ testCase "fullLang" $ fullLang [0..2::Int] 5 ]
+-- | Verify the size of the DAWG constructed from dataset no. 1.
+dataSet1DAWGSize :: Assertion
+dataSet1DAWGSize = do
+    D.numStates dawg @?= 11
+    D.numEdges dawg @?= 12
+  where
+    dawg = D.fromList dataSet1
+
+
+---------------------------------------------------------------------
+-- Sample Data
+---------------------------------------------------------------------
+
+
+-- | Sample dataset no. 1.  See also `dataSet1DAWGSize`.
+dataSet1 :: [(String, Int)]
+dataSet1 =
+    [ ("asdf", 1)
+    , ("asd", 1)
+    , ("adf", 1)
+    , ("df", 1)
+    , ("asdfg", 3)
+    , ("sdfg", 3) ]
